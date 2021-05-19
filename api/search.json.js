64d43cf0b6d00851a954f9799e7d13cff3bca362
Dillon Kearns <dillon@dillonkearns.com>
2021-05-18 21:43:32 -0700

    Add log for every time search.json is fetched.

--- api/search.json.js
+++ api/search.json.js
@@ -1,6 +1,7 @@
 const fetch = require("node-fetch");
 
 module.exports = async (req, res) => {
+  console.log("Fetching search.json");
   const response = await fetch(`https://package.elm-lang.org/search.json`);
   const docJson = await response.json();
   const secondsIn1Hour = 3600;
